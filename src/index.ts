6a51a5595f13e9e04c7bf481696a4822eebeed6d
yyhhenry <yyhhenry@foxmail.com>
2023-11-01 17:35:21 +0800

    feat: storage

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,75 @@
-type Theme = 'light' | 'dark';
-function assertNonNullable<T>(value: T, name?: string): asserts value is NonNullable<T> {
+function nonNullable<T>(value: T | null | undefined): T {
   if (value === null || value === undefined) {
-    throw new Error(`${name ?? 'The value'} is null or undefined`);
+    throw new Error('value is null or undefined');
   }
-}
-function setTheme(theme?: Theme) {
-  const url = new URL(location.href);
-  if (theme !== undefined) {
-    url.searchParams.set('theme', theme);
-  } else {
-    const themeNow = document.documentElement.getAttribute('data-theme');
-    if (!themeNow || themeNow === 'light') {
-      url.searchParams.set('theme', 'dark');
-    } else {
-      url.searchParams.set('theme', 'light');
-    }
-  }
-  location.href = url.href;
+  return value;
 }
 
-(() => {
-  const buttonList = document.querySelector<HTMLDivElement>(
-    '.CornerButtons div.CornerAnimayedFlex',
-  );
-  assertNonNullable(buttonList, 'buttonList');
-  buttonList.style.height = '90px';
-  const switchTheme = document.createElement('button');
-  switchTheme.setAttribute('data-tooltip', '切换主题');
-  switchTheme.setAttribute('data-tooltip-position', 'left');
-  switchTheme.setAttribute('data-tooltip-will-hide-on-click', 'true');
-  switchTheme.setAttribute('aria-label', '切换主题');
-  switchTheme.setAttribute('type', 'button');
-  const upButton = buttonList.firstChild;
-  assertNonNullable(upButton, 'upButton');
-  if (!(upButton instanceof HTMLButtonElement)) {
-    throw new Error('upButton is not HTMLButtonElement');
+const switchThemeSvg = `
+<svg width="24" height="24" viewBox="0 0 24 24"
+    aria-hidden="true" fill="currentColor"
+    stroke="currentColor">
+    <path fill-rule="evenodd" d="M2 2 L22 2 L2 22Z" clip-rule="evenodd"></path>
+    <path fill="none" d="M2 22 L22 2 L22 22Z"></path>
+</svg>
+`;
+const storageKey = 'zhihu-switch-theme';
+const buttonList = nonNullable(
+  document.querySelector<HTMLDivElement>('.CornerButtons div.CornerAnimayedFlex'),
+);
+buttonList.style.height = '90px';
+const upButton = nonNullable(buttonList.querySelector<HTMLButtonElement>('button'));
+
+type Theme = 'light' | 'dark';
+function isTheme(theme: unknown): theme is Theme {
+  return typeof theme === 'string' && (theme === 'light' || theme === 'dark');
+}
+
+function flipTheme(theme: Theme): Theme {
+  if (theme === 'light') {
+    return 'dark';
+  } else {
+    return 'light';
   }
-  switchTheme.className = upButton.className;
-  switchTheme.innerHTML = `
-  <svg width="24" height="24" viewBox="0 0 24 24" aria-hidden="true" fill="currentColor" stroke="currentColor"><path fill-rule="evenodd" d="M2 2 L22 2 L2 22Z" clip-rule="evenodd"></path><path fill="none" d="M2 22 L22 2 L22 22Z"></path></svg>
-  `;
-  switchTheme.addEventListener('click', () => setTheme());
-  buttonList.appendChild(switchTheme);
-})();
+}
+function getTheme(): Theme {
+  const theme = document.documentElement.getAttribute('data-theme');
+  if (!isTheme(theme)) {
+    throw new Error('html element data-theme is not a valid theme');
+  }
+  return theme;
+}
+function applyTheme(theme: Theme) {
+  if (theme !== getTheme()) {
+    setTimeout(() => {
+      const url = new URL(window.location.href);
+      url.searchParams.set('theme', theme);
+      location.href = url.href;
+    });
+  }
+}
+function storeTheme(theme: Theme) {
+  localStorage.setItem(storageKey, theme);
+}
+function getStoredTheme(): Theme {
+  const theme = localStorage.getItem(storageKey);
+  return isTheme(theme) ? theme : 'dark';
+}
+function switchTheme() {
+  const theme = flipTheme(getTheme());
+  applyTheme(theme);
+  storeTheme(theme);
+}
+
+function addSwitchThemeButton() {
+  const button = document.createElement('button');
+  button.setAttribute('data-tooltip', '切换主题');
+  button.setAttribute('data-tooltip-position', 'left');
+  button.className = upButton.className;
+  button.innerHTML = switchThemeSvg;
+  button.addEventListener('click', () => switchTheme());
+  buttonList.appendChild(button);
+}
+
+addSwitchThemeButton();
+applyTheme(getStoredTheme());
